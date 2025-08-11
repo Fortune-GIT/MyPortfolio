18ea97f708586bc7d798022fd7b1c805f7ab9c05
Fortune-GIT <anosike.fortune@gmail.com>
2025-08-11 02:47:56 +0100

    solving merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-<<<<<<< HEAD
 import Hero from './Components/Hero/Hero'
-=======
->>>>>>> origin/main
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-<<<<<<< HEAD
       <Hero/>
-=======
->>>>>>> origin/main
     </div>
   )
 }
